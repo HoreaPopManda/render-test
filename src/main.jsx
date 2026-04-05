8ca52e9d30ec367f84762138e3a6da4d72962981
horea <horeapopmanda@gmail.com>
2026-04-05 21:10:52 +1000

    first commit wrong project. this is the good commit

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Currency from "./Currency";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-//ReactDOM.createRoot(document.getElementById("root")).render(<Currency />);
